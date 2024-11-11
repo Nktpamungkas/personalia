92ca3e93fce3ff4308cc1f19703996f5649cfabd
Asep Rifki <rifkipauji993@gmail.com>
2024-11-11 08:32:15 +0700

    tambah menu untuk edit email dan user departerment lain di akun pak yohannes

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -3,15 +3,91 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class menu extends CI_Controller
 {
-    public function index()
-    {
-        $data['user'] = $this->db->get_where('user', array('name' =>
-        $this->session->userdata('name')))->row_array(); //Select * from user where name = '$name'
-        // echo 'selemat datang ' . $data['user']['name'];
-        $data['title'] = 'Menu Management';
-        $this->load->view('template/header', $data);
-        $this->load->view('setting/menuSetting', $data);
-        $this->load->view('menu/index');
-        $this->load->view('template/footer');
-    }
+	public function index()
+	{
+		$data['user'] = $this->db->get_where('user', array(
+			'name' =>
+				$this->session->userdata('name')
+		))->row_array(); //Select * from user where name = '$name'
+		// echo 'selemat datang ' . $data['user']['name'];
+		$data['title'] = 'Menu Management';
+		$this->load->view('template/header', $data);
+		$this->load->view('setting/menuSetting', $data);
+		$this->load->view('menu/index');
+		$this->load->view('template/footer');
+	}
+	public function edit_Password($no_scan)
+	{
+		$name = $this->input->post('name1');
+		$password = $this->input->post('old_password');
+		$email_user = $this->input->post('email');
+		$password_default = $this->input->post('password1');
+		$user = $this->db->get_where('user', array('name' => $name, 'ket' => 'HRIS'))->row_array();
+
+		if (password_verify($password, $user['password'])) {
+			$email = $this->input->post('email', true);
+			$password_default1 = $this->input->post('password1');
+			//array
+			$data = array(
+				'email' => $email,
+				'password' => password_hash($password_default1, PASSWORD_DEFAULT)
+			);
+			$this->db->where('no_scan', $no_scan);
+			$this->db->update('user', $data);
+
+			$this->sendPasswordUpdateEmail($email_user, $name, $password_default);
+			$this->session->set_flashdata('message', '<center class="alert alert-success" role="alert">Email dan password berhasil diperbarui.</center>');
+			redirect('menu/index');
+		} else {
+			$this->session->set_flashdata('message', '<center class="alert alert-danger" role="alert">Old Password Incorrect!</center>');
+			redirect('menu/index');
+		}
+	}
+	private function sendPasswordUpdateEmail($email_user, $name, $password_default)
+	{
+		$email_user = $this->input->post('email');
+		$this->load->library('phpmailer_lib');
+		$mail = $this->phpmailer_lib->load();
+
+		// SMTP configuration
+		$mail->isSMTP();
+		$mail->Host = 'mail.indotaichen.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = 'dept.it@indotaichen.com';
+		$mail->Password = 'Xr7PzUWoyPA';
+		$mail->SMTPSecure = 'TLS';
+		$mail->Port = 587;
+
+		// Email sender and reply-to details
+		$mail->setFrom('dept.it@indotaichen.com', 'Dept IT');
+		$mail->addReplyTo('dept.it@indotaichen.com', 'Dept IT');
+
+		// Recipient email
+		$mail->addAddress($email_user);
+
+		$mail->Subject = 'Informasi Akun HRIS Baru';
+		$mail->isHTML(true);
+
+		// Compose email content
+		$mailContent = "<html>
+                            <head></head>
+                            <body>
+                                Data Akun HRIS baru, sebagai berikut: <br>                              
+                                Email: $email_user <br>
+                                Username: $name <br>
+                                Password: $password_default <br>
+                                <br>
+                                Dimohon untuk login menggunakan username dan password tersebut, dan merubah password di <a href='https://online.indotaichen.com/personalia'>https://online.indotaichen.com/personalia</a> <br>
+                                <br>
+                                Terimakasih
+                            </body>
+                        </html>";
+
+		$mail->Body = $mailContent;
+
+		// Send email and handle errors
+		if (!$mail->send()) {
+			$this->session->set_flashdata('message', '<center class="alert alert-warning" role="alert"><b>Your Employee has been Verified but the email was not successfully sent.</b>' . $mail->ErrorInfo . '</center>');
+		}
+	}
 }
--- application/views/menu/index.php
+++ application/views/menu/index.php
@@ -1,62 +1,139 @@
-            <div class="col-sm-9">
-                <div class="content-panel">
-                    <table class="table table-striped table-advance table-hover">
-                        <h4><a href="<?= base_url('auth/registration'); ?>" class="btn btn-info"><i class=" fa fa-plus"></i>&nbsp;&nbsp;New Account</a></h4>
-                        <?= $this->session->flashdata('message'); ?>
-                        <hr>
-                        <thead>
-                            <tr>
-                                <th><i class="fa fa-user"></i> Nama</th>
-                                <th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
-                                <th><i class="fa fa-bookmark"></i> Department</th>
-                                <th><i class=" fa fa-edit"></i> Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                        $queryManagement = "SELECT * FROM user ORDER BY id ASC";
-                        $dataManagement = $this->db->query($queryManagement)->result_array();
-                        ?>
-                        <?php foreach ($dataManagement as $dm) : ?>
-                            <tr>
-                                <td>
-                                <a href="#"><img src="img/profile/<?= $dm['image']; ?>" class="img-circle" width="25" height="25"> &nbsp; <?= $dm['name']; ?></a>
-                                </td>
-                                <td class="hidden-phone">
-                                <?php
-                                    if ($dm['role_id'] == 1) {
-                                        echo "Administrator";
-                                    } elseif ($dm['role_id'] == 2) {
-                                        echo "Manager";
-                                    } elseif ($dm['role_id'] == 3) {
-                                        echo "Ast. Manager";
-                                    } elseif ($dm['role_id'] == 4)  {
-                                        echo "Staf Only";
-                                    } elseif ($dm['role_id'] == 5)  {
-                                        echo "Staf Master";
-                                    } else {
-                                        echo "Admin Department";
-                                    }
-                                ?>
-                                </td>
-                                <td><?= $dm['dept']; ?></td>
-                                <td><span class=""><?php
-                                        if ($dm['is_active'] == 1) {
-                                            echo '<a href=' . base_url('auth/nonActivated/') . $dm['id'] . ' class="label label-success label-mini">Activated</a>';
-                                        } else {
-                                            echo '<a href=' . base_url('auth/activated/') . $dm['id'] . ' class="label label-warning label-mini">Click here to Activated</a>';
-                                        } ?></span></td>
-                                <!-- <td>
-                                <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                                <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                                </td> -->
-                            </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </section>
+<div class="col-sm-9">
+	<div class="content-panel">
+		<table class="table table-striped table-advance table-hover">
+			<h4><a href="<?= base_url('auth/registration'); ?>" class="btn btn-info"><i
+						class=" fa fa-plus"></i>&nbsp;&nbsp;New Account</a></h4>
+			<?= $this->session->flashdata('message'); ?>
+			<hr>
+			<thead>
+				<tr>
+					<th><i class="fa fa-user"></i> Nama</th>
+					<th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
+					<th><i class="fa fa-bookmark"></i> Department</th>
+					<th><i class=" fa fa-edit"></i> Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				$queryManagement = "SELECT * FROM user ORDER BY id ASC";
+				$dataManagement = $this->db->query($queryManagement)->result_array();
+				?>
+				<?php foreach ($dataManagement as $dm): ?>
+					<tr>
+						<td>
+							<a href="#"><img src="img/profile/<?= $dm['image']; ?>" class="img-circle" width="25"
+									height="25"> &nbsp; <?= $dm['name']; ?></a>
+						</td>
+						<td class="hidden-phone">
+							<?php
+							if ($dm['role_id'] == 1) {
+								echo "Administrator";
+							} elseif ($dm['role_id'] == 2) {
+								echo "Manager";
+							} elseif ($dm['role_id'] == 3) {
+								echo "Ast. Manager";
+							} elseif ($dm['role_id'] == 4) {
+								echo "Staf Only";
+							} elseif ($dm['role_id'] == 5) {
+								echo "Staf Master";
+							} else {
+								echo "Admin Department";
+							}
+							?>
+						</td>
+						<td><?= $dm['dept']; ?></td>
+						<td><span class=""><?php
+						if ($dm['is_active'] == 1) {
+							echo '<a href=' . base_url('auth/nonActivated/') . $dm['id'] . ' class="label label-success label-mini">Activated</a>';
+						} else {
+							echo '<a href=' . base_url('auth/activated/') . $dm['id'] . ' class="label label-warning label-mini">Click here to Activated</a>';
+						} ?></span></td>
+						<td>
+							<?php if ($user['no_scan'] == 5221): ?>
+								<!-- Button to trigger password change modal -->
+								<button type="button" class="btn btn-primary" data-toggle="modal"
+									data-target="#registrationModal<?= $dm['id']; ?>">
+									Change Password
+								</button>
+							<?php endif; ?>
+						</td>
+						<!-- <td>
+								<button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+								<button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+								<button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+								</td> -->
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+	</div>
+</div>
+</div>
 </section>
+</section>
+<!-- Modal for changing password -->
+<?php foreach ($dataManagement as $dm): ?>
+	<div class="modal fade" id="registrationModal<?= $dm['id']; ?>" tabindex="-1" role="dialog"
+		aria-labelledby="modalResign" aria-hidden="true">
+		<form role="form" action="<?= base_url('Menu/edit_Password/' . $dm['no_scan']); ?>" method="POST">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h4 class="modal-title" id="myModalLabel">Change Password</h4>
+						<?php if ($this->session->flashdata('message')): ?>
+							<div id="errorMessage"><?php echo $this->session->flashdata('message'); ?></div>
+						<?php endif; ?>
+					</div>
+					<div class="modal-body">
+						<div class="login-wrap">
+							<div class="form-group">
+								<p>Username</p>
+								<input type="text" class="form-control form-control-user" name="name1"
+									value="<?= $dm['name']; ?>" readonly>
+							</div>
+							<div class="form-group">
+								<p>No Scan</p>
+								<input type="text" class="form-control form-control-user" name="no_scan"
+									value="<?= $dm['no_scan']; ?>" readonly>
+							</div>
+							<div class="form-group">
+								<p>Department</p>
+								<input type="text" class="form-control form-control-user" name="dept"
+									value="<?= $dm['dept']; ?>" readonly>
+							</div>
+							<div class="form-group">
+								<p>Email</p>
+								<input type="email" class="form-control form-control-user" name="email"
+									value="<?= $dm['email']; ?>" required>
+							</div>
+							<div class="form-group">
+								<p>Old Password</p>
+								<input type="password" class="form-control form-control-user" name="old_password"
+									placeholder="Old Password" required>
+								<?= form_error('old_password', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+							<div class="form-group">
+								<p>New Password</p>
+								<input type="password" class="form-control form-control-user" name="password1"
+									placeholder="New Password" required>
+								<?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+							<div class="form-group">
+								<p>Confirm Password</p>
+								<input type="password" class="form-control form-control-user" name="password2"
+									placeholder="Repeat New Password" required>
+								<?= form_error('password2', '<small class="text-danger pl-3">', '</small>'); ?>
+							</div>
+							<button type="submit" class="btn btn-theme btn-block"><i class="fa fa-lock"></i>
+								Save</button>
+							<button type="button" class="btn btn-default btn-block" data-dismiss="modal">Back</button>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</form>
+	</div>
+
+<?php endforeach; ?>
